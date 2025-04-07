e36be37bc4a3af87b9f22d2ad24cf673dc22eea0
unset <pedro.halo.ph@gmail.com>
2025-04-07 07:49:45 -0300

    url

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,7 +1,7 @@
 // URL API
 
 // Variável Global
-var BASE_URL = "http://192.168.1.119:5000";
+var BASE_URL = "http://192.168.1.114:5000";
 
 // Abrir e fechar barra lateral
 const sanduiche = $("#sanduicheHeader");
