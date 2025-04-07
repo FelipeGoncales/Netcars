4bf210f6ced8a19af433b8addfd598526177ba8f
FelipeGoncales <felipeggoncales22@gmail.com>
2025-04-07 17:24:52 -0300

    editar imagem carro

--- assets/js/anuncio-carro.js
+++ assets/js/anuncio-carro.js
@@ -1,3 +1,57 @@
+// Função de exibir a mensagem para evitar repetir código
+function alertMessage(text, type) {
+    $('#divAlertMessage').empty().css('display', 'flex');
+    $('#divAlertMessage').css('display', 'flex')
+
+    let bgColor = type === 'success' ? '#0bd979' : '#f71445';
+
+    $('<p>')
+        .addClass('alertMessage')
+        .text(text)
+        .css('background-color', bgColor)
+        .appendTo('#divAlertMessage')
+        .hide()
+        .fadeIn(400)
+        .delay(3500)
+        .fadeOut(400);
+}
+
+// Declarando variável global
+var tipoUser;
+
+// Obter tipo de usuário
+function obterTipoUser() {
+    const dadosUser = JSON.parse(localStorage.getItem('dadosUser'));
+
+    const token = dadosUser.token;
+
+    if (!token) {
+        localStorage.removeItem('dadosUser');
+        localStorage.setItem('mensagem', JSON.stringify({
+            "error": "Sessão não iniciada."
+        }))
+        window.location.href = 'login.html';
+    }
+
+    return $.ajax({
+        url: `${BASE_URL}/obter_tipo_usuario`,
+        headers: {
+            "Authorization": "Bearer " + token
+        },
+        success: function (response) {
+            tipoUser = response.tipo_usuario;
+            return;
+        },
+        error: function (response) {
+            localStorage.removeItem('dadosUser');
+            localStorage.setItem('mensagem', JSON.stringify({
+                "error": response.responseJSON.error
+            }))
+            return window.location.href = "login.html";
+        }
+    })
+}
+
 // Dicionário com a URL da foto da logo das marcas de carro
 const logo_carros = {
     "Acura": "assets/img/logo-carro/acura.png",
@@ -44,6 +98,43 @@ const logo_carros = {
     "Volkswagen": "assets/img/logo-carro/volkswagem.png",
     "Volvo": "assets/img/logo-carro/volvo.png"
 }
+
+// Função para funcionar filtro de categorias
+$(document).ready(function () {
+    $(".div-modelos a").on("click", function (e) {
+        e.preventDefault(); // Previne o comportamento padrão do link
+
+        // Pega a categoria da moto
+        const categoria = $(this).attr("categoria");
+        const tipo = $(this).attr("tipo-veiculo");
+
+        // Salva no localStorage
+        localStorage.setItem("tipo-veiculo", tipo);
+        localStorage.setItem("filtro-categoria", categoria);
+
+        // Redireciona para a página de veículos
+        window.location.href = "veiculos.html";
+    });
+});
+
+// Função para funcionar filtro de marcas
+$(document).ready(function () {
+    $(".a-marcas-car").on("click", function (e) {
+
+        e.preventDefault(); // Previne o comportamento padrão do link
+
+        localStorage.setItem('tipo-veiculo', 'carro');
+
+        // Pegamos o id do elemento clicado
+        const marca = $(this).attr("marca");
+
+        // Salvamos no localStorage para usar na próxima página
+        localStorage.setItem("filtro-marca", marca);
+
+        // Redireciona para a página de veículos
+        window.location.href = "veiculos.html";
+    });
+});
 
 // Função para inicializar o carrossel
 function carregarOwlCarrossel() {
@@ -252,41 +343,6 @@ function extrairNumeros(valor) {
 // Adicionando formatação de preço
 formatarPreco('#input-preco-venda');
 
-var tipoUser;
-
-// Obter tipo de usuário
-function obterTipoUser() {
-    const dadosUser = JSON.parse(localStorage.getItem('dadosUser'));
-
-    const token = dadosUser.token;
-
-    if (!token) {
-        localStorage.removeItem('dadosUser');
-        localStorage.setItem('mensagem', JSON.stringify({
-            "error": "Sessão não iniciada."
-        }))
-        window.location.href = 'login.html';
-    }
-
-    return $.ajax({
-        url: `${BASE_URL}/obter_tipo_usuario`,
-        headers: {
-            "Authorization": "Bearer " + token
-        },
-        success: function (response) {
-            tipoUser = response.tipo_usuario;
-            return;
-        },
-        error: function (response) {
-            localStorage.removeItem('dadosUser');
-            localStorage.setItem('mensagem', JSON.stringify({
-                "error": response.responseJSON.error
-            }))
-            return window.location.href = "login.html";
-        }
-    })
-}
-
 // Alterar botão
 async function alterarBotao() {
     const dadosUser = JSON.parse(localStorage.getItem('dadosUser'))
@@ -383,7 +439,10 @@ $(document).ready(async function () {
             // Limpa o conteúdo atual do carrossel
             divCarrossel.empty();
 
+            const dt = new DataTransfer(); // Cria um objeto DataTransfer
+
             for (const imagem of urlImagens) {
+
                 // CSS para ficar todas as fotos do mesmo tamanho
                 const divImg = $('<div></div>')
                     .css({
@@ -392,18 +451,92 @@ $(document).ready(async function () {
                         "min-height": "400px",
                         "overflow": "hidden"
                     })
-                divImg.append($('<img>').attr('src', imagem).css({
+
+                // Adicionando a imagem
+                const img = $('<img>').attr('src', imagem).css({
+                    "height": "100%",
+                    "min-width": "100%",
+                    "width": "auto",
+                    "display": "block",
                     "position": "absolute",
                     "top": "50%",
                     "left": "50%",
-                    "transform": "translate(-50%, -50%)",
-                    "height": "100%",
-                    "min-width": "100%",
-                    "width": "auto"
-                }))
+                    "transform": "translate(-50%, -50%)"
+                });
 
+                // Adicionando a imagem e o overlay à div principal
+                divImg.append(img);
+
+                // Input final placa
+                await obterTipoUser();
+
+                if (tipoUser in [1, 2]) {
+                    // Criando o overlay
+                    const overlay = $('<div></div>').css({
+                        "cursor": "pointer",
+                        "position": "absolute",
+                        "top": "0",
+                        "left": "0",
+                        "width": "100%",
+                        "height": "100%",
+                        "background-color": "rgba(0, 0, 0, 0.5)", // Cor escura com transparência
+                        "display": "flex",
+                        "align-items": "center",
+                        "justify-content": "center",
+                        "flex-direction": "column",
+                        "opacity": "0",
+                        "transition": "opacity 0.3s ease"
+                    }).addClass('overlay-img-carrossel');
+
+                    // Adicionando o ícone e o texto ao overlay
+                    const icon = $('<i class="fa-solid fa-arrow-up-from-bracket"></i>').css({
+                        "font-size": "2rem",
+                        "color": "#FFF",
+                        "margin-bottom": "0.5rem"
+                    });
+                    const text = $('<span>Editar imagens</span>').css({
+                        "color": "#FFF",
+                        "font-size": "1rem"
+                    });
+
+                    overlay.append(icon, text);
+
+                    // Adicionando a imagem e o overlay à div principal
+                    divImg.append(overlay);
+
+                    // Adicionando eventos de hover para mostrar/ocultar o overlay
+                    divImg.hover(
+                        function() {
+                            overlay.css("opacity", "1");
+                        },
+                        function() {
+                            overlay.css("opacity", "0");
+                        }
+                    );
+                }
+
+                // Adicionando divImg ao carrossel ou ao elemento desejado
                 divCarrossel.append(divImg);
+
+                // Realiza o fetch da imagem e obtém o Blob
+                const response = await fetch(imagem);
+                const blob = await response.blob();
+
+                // Extrai o nome do arquivo da URL
+                const fileName = imagem.substring(imagem.lastIndexOf('/') + 1);
+
+                // Cria um objeto File com o Blob
+                const file = new File([blob], fileName, { type: blob.type });
+                
+                // Adiciona o arquivo ao DataTransfer
+                dt.items.add(file);
             }
+
+            // Atribui os arquivos ao input file
+            document.getElementById('upload-imagem').files = dt.files;
+
+            // Carregar a miniatura das imagens atuais
+            carregarPreviewImg();
 
             // Inicializa o carrossel após adicionar os itens
             carregarOwlCarrossel();
@@ -530,9 +663,148 @@ $(document).ready(async function () {
     });
 })
 
+// Preview Imagens
+
+// Variável global para armazenar os arquivos
+let currentFiles = new DataTransfer();
+
+function carregarPreviewImg() {
+    const inputFile = document.getElementById('upload-imagem');
+    const newFiles = Array.from(inputFile.files);
+    currentFiles = new DataTransfer();
+    
+    // Adiciona novos arquivos ao DataTransfer
+    newFiles.forEach(file => currentFiles.items.add(file));
+    
+    // Atualiza o input com os novos arquivos
+    inputFile.files = currentFiles.files;
+
+    const previewContainer = $("#preview-container");
+
+    previewContainer.empty();
+
+    Array.from(inputFile.files).forEach((file, index) => {
+        if (file.type.startsWith("image/")) {
+            const reader = new FileReader();
+
+            reader.onload = function(e) {
+                const imgContainer = $('<div>')
+                    .addClass('preview-item')
+                    .css('background-image', `url(${e.target.result})`);
+
+                const removeBtn = $('<i>')
+                    .addClass('fa-solid fa-xmark remove-btn')
+                    // No callback de remoção, em vez de currentFiles.items.remove(index):
+                    .on('click', function() {
+                        // Cria um novo DataTransfer para armazenar apenas os arquivos que você quer manter
+                        let dtTemp = new DataTransfer();
+                        const inputFiles = document.getElementById('upload-imagem').files;
+                        
+                        // Reconstroi a lista, pulando o arquivo removido (identificado pelo index atual)
+                        Array.from(inputFiles).forEach((file, i) => {
+                            if(i !== index){
+                                dtTemp.items.add(file);
+                            }
+                        });
+                        
+                        // Atualiza o input com o novo FileList
+                        document.getElementById('upload-imagem').files = dtTemp.files;
+                        
+                        // Atualiza o preview para refletir a nova lista de arquivos
+                        carregarPreviewImg();
+                    });
+
+                imgContainer.append(removeBtn);
+                previewContainer.append(imgContainer);
+            };
+
+            reader.readAsDataURL(file);
+        }
+    });
+}
+
+// Adicionar o evento carregar preview toda vez que input file for alterado
+$("#upload-imagem").on("change", function(event) {
+    carregarPreviewImg();
+});
+
+// Função de fechar modal de editar
+function fecharModalEditarImagem() {
+    $('#modal-editar-imagem').css('display', 'none');
+    $('#overlay-bg').css('display', 'none');
+}
+
+// Função de abrir modal de editar
+function abrirModalEditarImagem() {
+    $('#modal-editar-imagem').css('display', 'flex');
+    $('#overlay-bg').css('display', 'flex');
+}
+
+// Fechar modal editar imagem ao clicar no X
+$('#closeModalEditarImagem').click(function() {
+   fecharModalEditarImagem();
+})
+
+// Associando o evento de clique usando delegação de eventos
+$(document).on('click', '.overlay-img-carrossel', function() {
+    abrirModalEditarImagem();
+});
+
+// Editar imagens
+
+$('#modal-editar-imagem').on('submit', function(e) {
+    e.preventDefault();
+
+    // Caso queira inspecionar os arquivos:
+    const files = $('#upload-imagem')[0].files;
+
+    // Cria um objeto FormData
+    let formDataImg = new FormData();
+
+    // Adiciona as imagens ao objeto
+    for (let i = 0; i < files.length; i++) {
+        formDataImg.append('imagens', files[i]);
+    }
+
+    // Requisição para editar imagens
+    $.ajax({
+        url: `${BASE_URL}/carro/editar_img/${id_carro}`,
+        method: 'PUT',
+        data: formDataImg,
+        headers: {
+            "Authorization": "Bearer " + JSON.parse(localStorage.getItem('dadosUser')).token
+        },
+        processData: false,
+        contentType: false,
+        success: function() {
+            // Define uma mensagem de sucesso para quando recarregar a página exibir ao usuário
+            localStorage.setItem('mensagemEditado', 'Imagens do veículo editadas com sucesso!');
+
+            // Recarrega a página para que as aplicações sejam feitas
+            window.location.reload();
+        }, 
+        error: function(response) {
+            // Caso dê erro, exibe a mensagem
+            alertMessage(response.responseJSON.error, 'error');
+        }
+    })
+})
+
+// Caso dê certo o editar, exibir mensagem de sucesso ao abrir a página
+$(document).ready(function() {
+    // Obtêm o item do local storage
+    const mensagemEditado = localStorage.getItem('mensagemEditado');
+
+    // Caso exista alguma mensagem no local storage
+    if (mensagemEditado) {
+        // Mensagem de sucesso
+        alertMessage(mensagemEditado, 'success');
+        // Remove a mensagem depois de usá-la
+        localStorage.removeItem('mensagemEditado');
+    }
+})
 
 // Cancelar reserva
-
 $('#cancelar-reserva').click(function () {
     Swal.fire({
         title: "Você tem certeza?",
@@ -569,7 +841,6 @@ $('#cancelar-reserva').click(function () {
 })
 
 // Deletar veículo
-
 $('#deletar-veiculo').click(function () {
     Swal.fire({
         title: "Você tem certeza?",
@@ -609,7 +880,6 @@ $('#deletar-veiculo').click(function () {
 })
 
 // Reservar carro
-
 $('#reservar-btn').click(function () {
     Swal.fire({
         title: "Deseja reservar esse veículo?",
@@ -668,7 +938,7 @@ $("#editarAnuncio").on("click", function () {
     // Verifica se os inputs não estão visíveis
     if (editarOn === false) {
         editarOn = true;
-        
+
         // Alterar ícone do botão de editar para X
         $(this).removeClass('fa-pencil').addClass('fa-xmark');
 
@@ -682,20 +952,23 @@ $("#editarAnuncio").on("click", function () {
 
         $("input, select").prop("disabled", false);
 
-        $("input, select").css("display", "block");
-    
+        $("input, select").css("display", "flex");
+
         // Barra (/) do ano
         $('#barra-ano-mirror').css('display', 'none');
         $('#barra-ano-select').css('display', 'flex');
-    
+
         // Traço (-) da cidade
         $('#dash-cidade-mirror').css('display', 'none');
         $('#dash-cidade-select').css('display', 'flex');
-    
+
         $("p.input-mirror, p.select-mirror").css("display", "none");
-    
+
         // Habilita o botão de salvar alterações
         $('#salvar-alteracoes').prop('disabled', false);
+
+        // Evita que o input de imagem seja alterado
+        $('#upload-imagem').css('display', 'none').prop('disabled', false);
     } else {
         editarOn = false;
 
@@ -705,17 +978,17 @@ $("#editarAnuncio").on("click", function () {
         $("input, select").prop("disabled", true);
 
         $("input, select").css("display", "none");
-    
+
         // Barra (/) do ano
         $('#barra-ano-mirror').css('display', 'flex');
         $('#barra-ano-select').css('display', 'none');
-    
+
         // Traço (-) da cidade
         $('#dash-cidade-mirror').css('display', 'flex');
         $('#dash-cidade-select').css('display', 'none');
-    
+
         $("p.input-mirror, p.select-mirror").css("display", "flex");
-    
+
         // Habilita o botão de salvar alterações
         $('#salvar-alteracoes').prop('disabled', true);
 
@@ -742,6 +1015,9 @@ $("#editarAnuncio").on("click", function () {
         }
 
         selectLicenciado.val(valorLicenciado);
+
+        // Evita que o input de imagem seja alterado
+        $('#upload-imagem').css('display', 'none').prop('disabled', false);
     }
 });
 
@@ -794,6 +1070,9 @@ $("#salvar-alteracoes").on("click", function (e) {
             // Se necessário, desabilite os inputs novamente e atualize os "mirrors"
             $("input").prop("disabled", true);
 
+            // Evita que o input de imagem seja alterado
+            $('#upload-imagem').css('display', 'none').prop('disabled', false);
+
             // Passar o valor dos inputs pros mirrors
             carregarInputs();
 
@@ -806,7 +1085,7 @@ $("#salvar-alteracoes").on("click", function (e) {
         error: function (response) {
             // Reabilita o botão caso dê erro
             $('#salvar-alteracoes').prop('disabled', false);
-            
+
             // Exibe mensagem de erro
             Swal.fire({
                 title: "Erro",
